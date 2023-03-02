faea376d065b6f90cda299af5af50554dd5f737b
Raymond <Raymond07xia@gmail.com>
2023-03-02 15:50:26 -0800

    temp

--- app/src/main/java/edu/ucsd/cse110/sharednotes/model/NoteRepository.java
+++ app/src/main/java/edu/ucsd/cse110/sharednotes/model/NoteRepository.java
@@ -7,6 +7,7 @@ import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Observer;
 
+<<<<<<< HEAD
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.Executors;
@@ -16,6 +17,11 @@ import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Request;
 import okhttp3.Response;
+=======
+import java.time.Instant;
+import java.util.List;
+import java.util.concurrent.ScheduledExecutorService;
+>>>>>>> 64dcf988f423c03396e5e975883661bbce51874a
 
 public class NoteRepository {
     private final NoteDao dao;
@@ -43,7 +49,8 @@ public class NoteRepository {
 
         Observer<Note> updateFromRemote = theirNote -> {
             var ourNote = note.getValue();
-            if (ourNote == null || ourNote.updatedAt < theirNote.updatedAt) {
+            if (theirNote == null) return; // do nothing
+            if (ourNote == null || ourNote.version < theirNote.version) {
                 upsertLocal(theirNote);
             }
         };
@@ -70,11 +77,10 @@ public class NoteRepository {
 
     public LiveData<List<Note>> getAllLocal() {
         return dao.getAll();
-
     }
 
     public void upsertLocal(Note note) {
-        note.updatedAt = System.currentTimeMillis();
+        note.version = note.version + 1;
         dao.upsert(note);
     }
 
@@ -94,10 +100,12 @@ public class NoteRepository {
         // TODO: Set up polling background thread (MutableLiveData?)
         // TODO: Refer to TimerService from https://github.com/DylanLukes/CSE-110-WI23-Demo5-V2.
 
-        // Start by fetching the note from the server ONCE.
+        // Start by fetching the note from the server _once_ and feeding it into MutableLiveData.
         // Then, set up a background thread that will poll the server every 3 seconds.
+
         // You may (but don't have to) want to cache the LiveData's for each title, so that
         // you don't create a new polling thread every time you call getRemote with the same title.
+<<<<<<< HEAD
 //        LiveData<Note> remoteNote = getSynced(title);
 
         // Then, set up a background thread that will poll the server every 3 seconds.
@@ -117,6 +125,11 @@ public class NoteRepository {
             Log.e("NoteAPI", "Error getting note to server", e);
         }
         return null;
+=======
+        // You don't need to worry about killing background threads.
+
+        throw new UnsupportedOperationException("Not implemented yet");
+>>>>>>> 64dcf988f423c03396e5e975883661bbce51874a
     }
 //        throw new UnsupportedOperationException("Not implemented yet");
 
