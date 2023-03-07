e54d8e6ec88bc1e7a5793a856c19efa3b8b2ca49
Raymond <Raymond07xia@gmail.com>
2023-03-06 16:50:55 -0800

    1

--- app/src/main/java/edu/ucsd/cse110/sharednotes/model/NoteRepository.java
+++ app/src/main/java/edu/ucsd/cse110/sharednotes/model/NoteRepository.java
@@ -8,8 +8,11 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Observer;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.Call;
@@ -24,7 +27,9 @@ import java.util.concurrent.ScheduledExecutorService;
 public class NoteRepository {
     private final NoteDao dao;
     private final NoteAPI api;
-
+//    private ScheduledFuture<?> pollingFuture;
+    private Map<String, ScheduledFuture<?>> pollingFutures = new HashMap<>();
+    private ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
     public NoteRepository(NoteDao dao) {
         this.dao = dao;
         this.api=NoteAPI.provide();
@@ -86,6 +91,7 @@ public class NoteRepository {
 
     public void deleteLocal(Note note) {
         dao.delete(note);
+        String title = note.title;
     }
 
     public boolean existsLocal(String title) {
@@ -95,36 +101,72 @@ public class NoteRepository {
     // Remote Methods
     // ==============
 
+//    public LiveData<Note> getRemote(String title) {
+//        // TODO: Implement getRemote!
+//        // TODO: Set up polling background thread (MutableLiveData?)
+//        // TODO: Refer to TimerService from https://github.com/DylanLukes/CSE-110-WI23-Demo5-V2.
+//
+//        // Start by fetching the note from the server _once_ and feeding it into MutableLiveData.
+//        // Then, set up a background thread that will poll the server every 3 seconds.
+//
+//        // You may (but don't have to) want to cache the LiveData's for each title, so that
+//        // you don't create a new polling thread every time you call getRemote with the same title.
+//
+//        MutableLiveData<Note> noteLiveData = new MutableLiveData<>();
+//        Note note = api.get(title);
+//// && note.version > localNote.version
+//        Note localNote = getLocal(title).getValue();
+////        if(note!=null){
+////            upsertLocal(note);
+////            noteLiveData.postValue(note);
+////        }
+//        if (pollingFuture != null) {
+//            pollingFuture.cancel(true);
+//        }
+//        ScheduledExecutorService temp = Executors.newSingleThreadScheduledExecutor();
+//        temp.scheduleAtFixedRate(()->{
+//            Note pulledNote = api.get(title);
+//            if (pulledNote != null) {
+//                upsertLocal(pulledNote);
+//                noteLiveData.postValue(pulledNote);
+//            }
+//        },0,3,TimeUnit.SECONDS);
+//
+//        return noteLiveData;
+//
+//    }
+
+
     public LiveData<Note> getRemote(String title) {
-        // TODO: Implement getRemote!
-        // TODO: Set up polling background thread (MutableLiveData?)
-        // TODO: Refer to TimerService from https://github.com/DylanLukes/CSE-110-WI23-Demo5-V2.
-
-        // Start by fetching the note from the server _once_ and feeding it into MutableLiveData.
-        // Then, set up a background thread that will poll the server every 3 seconds.
-
-        // You may (but don't have to) want to cache the LiveData's for each title, so that
-        // you don't create a new polling thread every time you call getRemote with the same title.
-
         MutableLiveData<Note> noteLiveData = new MutableLiveData<>();
         Note note = api.get(title);
-// && note.version > localNote.version
+
         Note localNote = getLocal(title).getValue();
-        if(note!=null){
+        if (note != null && (localNote == null || note.version > localNote.version)) {
             upsertLocal(note);
-            noteLiveData.postValue(note);
         }
-        ScheduledExecutorService temp = Executors.newSingleThreadScheduledExecutor();
-        temp.scheduleAtFixedRate(()->{
-            Note tempN = api.get(title);
-            if (tempN != null) {
-                upsertLocal(tempN);
-                noteLiveData.postValue(tempN);
+
+        ScheduledFuture<?> pollingFuture = pollingFutures.get(title);
+        if (pollingFuture != null) {
+            // A polling task already exists for this title, cancel it and remove it from the map
+            pollingFuture.cancel(true);
+            pollingFutures.remove(title);
+        }
+
+        pollingFuture = executor.scheduleAtFixedRate(() -> {
+            Note tempNote = api.get(title);
+            if (tempNote != null) {
+                if (localNote == null || tempNote.version > localNote.version) {
+                    upsertLocal(tempNote);
+                }
+                noteLiveData.postValue(tempNote);
             }
-        },0,3,TimeUnit.SECONDS);
+        }, 0, 3, TimeUnit.SECONDS);
+
+        // Add the new polling task to the map
+        pollingFutures.put(title, pollingFuture);
 
         return noteLiveData;
-
     }
 
 
